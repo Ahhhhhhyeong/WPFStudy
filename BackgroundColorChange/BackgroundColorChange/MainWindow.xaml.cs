b17ce50b005ee2d4889990993dc03a922bc2cf89
Ahhhhhhyeong <dkdud081@gmail.com>
2019-12-18 16:11:51 +0900

    used binding

---
+++ BackgroundColorChange/BackgroundColorChange/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BackgroundColorChange
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public System.Windows.Media.Color MyColor { get; set; }
+        private void red_Click(object sender, RoutedEventArgs e)
+        {
+            color.Text = red.Content.ToString();
+        }
+
+        private void blue_Click(object sender, RoutedEventArgs e)
+        {
+            color.Text = blue.Content.ToString();
+        }
+
+        private void yellow_Click(object sender, RoutedEventArgs e)
+        {
+            color.Text = yellow.Content.ToString();
+        }
+    }
+}
